8c292c17d368649fc2a23a5b5037a88bacbbbf33
amitOodles <amit.k@oodlestechnologies.com>
2016-07-15 11:07:08 +0530

    ss change $1

--- js/WithSSCalculatorService.js
+++ js/WithSSCalculatorService.js
@@ -4,8 +4,6 @@ app.service('WithSSCalculator', ['TaxRateCalculator','SGCRate','AgeCalculator',f
       taxFreePercent,netReturnInAccumulation,netReturnInPension,minTakeHomePay){
         var financialYear=datePension.getFullYear()+1;
 
-        var ddArray = [0.04,0.05,0.06,0.07,0.08,0.09,0.10];
-
         var nrpSqrt = Math.sqrt(1 + (netReturnInPension/100)) - 1;
 
         var cses =  currentSalaryExcludeSuper;
@@ -75,7 +73,7 @@ app.service('WithSSCalculator', ['TaxRateCalculator','SGCRate','AgeCalculator',f
         var unattainableTHP =  true;
 
         for(i=400;i<=1000;i=i+5){
-          for(ss=0;ss<=upperSS;ss=ss+10){
+          for(ss=0;ss<=upperSS;ss=ss+1){
 
         var concessionalContribution = validEmployerContribution + ss;
 
