aa057eae703c06039ba35aa61219f0a6233e1ec8
oodlesTickoo <himanshu.khurana@oodlestechnologies.com>
2017-06-08 15:45:29 +0530

    TTR updated folder 6-8-17

--- js/hc/chartService.js
+++ js/hc/chartService.js
@@ -1,6 +1,6 @@
 app.service('ChartServiceHc',function(){
   this.createChart = function(finalWSS,finalSS,taxSaving){
-    
+
     Highcharts.setOptions({lang: {
             thousandsSep: ','
         }});
@@ -22,11 +22,19 @@ app.service('ChartServiceHc',function(){
     // Create the chart
     $('#container').highcharts({
         chart: {
-            type: 'column'
+            type: 'column',
+            options3d: {
+              enabled: true,
+                 alpha: 7,
+                 beta: 18,
+                 depth: 47,
+                 viewDistance: 25
+   }
         },
         title: {
             text: 'Transition to Retirement'
         },
+        colors: ['#071520', '#0d2f47', '#195c8d', '#1a74b7', '#2599f0'],
         exporting:{
             enabled:false
         },
@@ -37,7 +45,8 @@ app.service('ChartServiceHc',function(){
             type: 'category',
             labels:{
                 autoRotation : false,
-            }
+            },
+            gridLineColor: 'transparent',
         },
         yAxis: {
             title: {
